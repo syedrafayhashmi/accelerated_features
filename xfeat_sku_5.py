8b394612fea34c1cd01db707c563aec51e372343
syedrafayhashmi <rafaysidsid@gmail.com>
2024-07-10 01:39:38 +0200

    dir fixes

--- xfeat_sku_5.py
+++ xfeat_sku_5.py
@@ -33,10 +33,10 @@ def warp_corners_and_draw_matches(ref_points, dst_points, img1, img2):
 
 xfeat = XFeat()
 dataset_name = "sku100k_v2"
-query_imgs_dir = f"./hlcv-project-datasets/{dataset_name}/query_images"
-class_imgs_dir = f"./hlcv-project-datasets/{dataset_name}/reference_images"
+query_imgs_dir = f"./datasets/{dataset_name}/query_images"
+class_imgs_dir = f"./datasets/{dataset_name}/reference_images"
 threshold = 5
-output_dir_path = f"./hlcv-project-datasets/output/{dataset_name}/top_{threshold}"
+output_dir_path = f"./datasets/output/{dataset_name}/top_{threshold}"
 
 query_img_path = os.listdir(query_imgs_dir)
 class_img_path = os.listdir(class_imgs_dir)
